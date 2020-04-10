6bec431f0541bcc6ebf6b71bff09ceef2a1c61ca
vitorgdev <vitorg.dev@gmail.com>
2020-04-10 19:12:23 -0300

    JOB

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = require('./src/config/express')();
+const port = app.get('port');
 
-// Our function which adds two numbers and returns the result
-const addNumbers = (firstNumber, secondNumber) => {
-  //   check that input is a number
-  if (typeof (Number(firstNumber)) !== 'number' || typeof (Number(secondNumber)) !== 'number') {
-    return 'Values should be integer or numbers'
-  }
-  return Number(firstNumber) + Number(secondNumber);
-}
-
-// Destructure our bodyParser methods
-const { urlencoded, json } = bodyParser;
-const port = process.env.PORT || 8080;
-// intialize our express app
-const app = express();
-
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(json());
-app.use(urlencoded({ extended: false }));
-
-// end point to add numbers
-app.post('/api/add', (req, res) => {
-  const { firstNumber, secondNumber } = req.body;
-  const result = addNumbers(firstNumber, secondNumber);
-  return res.status(200).send({
-    result
-  });
-});
-// app entry point
+// RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to our glorious app',
 }));
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
-
-app.listen(port, (err) => {
-  if (!err) {
-    console.log(`App started on port ${port}`);
-  } else {
-    console.log(err);
-  }
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
 });
-
-module.exports = app;
