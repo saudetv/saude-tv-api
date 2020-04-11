63e32baed5a6cf16d2fb14c9f94352a3437f7677
vitorgdev <vitorg.dev@gmail.com>
2020-04-10 21:58:17 -0300

    .env config

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ db.connect();
 
 const routes = require("./src/routes");
 
-// process.env.NODE_ENV = 'production';
-
 require('dotenv').config({
-  path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
+  path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env.production"
 })
 
 const app = express();
