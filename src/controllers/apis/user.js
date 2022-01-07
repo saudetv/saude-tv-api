57539eace534b49c9430e24cf3cadf688741c4a5
Vitor Meira Gomes <vitormeiragomes@MacBook-Air-de-SKEELO.local>
2022-01-07 18:17:35 -0300

    finalizado

--- src/controllers/apis/user.js
+++ src/controllers/apis/user.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const userServiceClass = require('../../services/users');
 const userService = new userServiceClass;
+const authService = require("../../services/auth");
 const authMiddleware = require('../../middleware/auth');
 const passport = require("passport");
 let router = express.Router();
 
 router.get('/', authMiddleware, userService.index);
 router.get('/:id', authMiddleware, userService.show);
+router.get("/check", authMiddleware, authService.sendUser);
 router.post('/', userService.store);
 router.put('/:id', authMiddleware, userService.update);
 router.delete('/:id', authMiddleware, userService.destroy);
