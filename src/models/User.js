88004fab9d2f8973ab44293ac3f71642c67047ff
Matheus <matheus@kokua.digital>
2022-01-07 18:11:59 -0300

    CU-1ug3brd - Terminal - Frontend

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String },
+  password: { type: String, required: true },
   type: { type: String, required: true },
   name: { type: String, required: true, trim: true },
   createdAt: { type: Date, default: Date.now() },
