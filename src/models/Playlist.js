5669ff5bfbb63e38372fe737a16a74de86c712aa
Matheus <matheus@kokua.digital>
2022-01-18 14:41:43 -0300

    ✅CU-1ug3brd - Playlist models - Backend

--- src/models/Playlist.js
+++ src/models/Playlist.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const PlaylistSchema = new mongoose.Schema({
-  adMessager: { type: String, required: true },
-  adDescription: { type: String },
+  categoryItems: { type: Array, required: true },
+  filterItems: { type: Boolean },
+  search: { type: String, required: true },
+  layout: { type: String, required: true },
+  description: { type: String, required: true },
+  quantity: { type: String, required: true },
+  checkbox: { type: Boolean },
+  checkbox2: { type: Boolean },
+  name: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Playlist", PlaylistSchema);
