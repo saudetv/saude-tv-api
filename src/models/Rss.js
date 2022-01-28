61a47f76169652784c011649acdaa0c32e81c440
Matheus <matheus@kokua.digital>
2022-01-28 14:14:24 -0300

    CU-1w3mczj - test - Backend

--- src/models/Rss.js
+++ src/models/Rss.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const RssSchema = new mongoose.Schema({
   name: { type: String },
   template: { type: Array },
+  
 });
 
 module.exports = mongoose.model("Rss", RssSchema);
