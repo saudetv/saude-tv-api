005ae481a926811c97c3b1aa12001fcf17009edf
Matheus <matheus@kokua.digital>
2022-01-28 13:17:28 -0300

    CU-1ug3brd - Conta felipe@kokua.digital  - Backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,24 @@
 const auth = require("../middleware/auth");
 
-const simpleGit = require('simple-git')();
-const v1ApiController = require('./apis/v1');
-const authController = require('../controllers/apis/auth');
-var versionGit = '0.0.1'
+const simpleGit = require("simple-git")();
+const v1ApiController = require("./apis/v1");
+const authController = require("../controllers/apis/auth");
+var versionGit = "0.0.1";
 
-var swaggerUi = require('swagger-ui-express'),
-  swaggerDocument = require('../config/swagger.json');
+var swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("../config/swagger.json");
 
 module.exports = {
   async register(app) {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    app.use('/auth', authController);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use("/auth", authController);
     // app.use(auth);
-    app.use('/v1', v1ApiController);
+    app.use("/v1", v1ApiController);
     app.get("/", ({ res }) => {
       res.json({
         appName: "SAUDE TV! API",
-        currentVersionApi: versionGit
+        currentVersionApi: versionGit,
       });
     });
-  }
+  },
 };
