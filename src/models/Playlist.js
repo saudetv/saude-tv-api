a685db5dede1ce2d994f7a608623f85e537c99d6
Matheus <matheus@kokua.digital>
2022-01-28 13:52:13 -0300

    CU-1w3mczj - conta felipe@kokua.digital - Backend

--- src/models/Playlist.js
+++ src/models/Playlist.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
 const PlaylistSchema = new mongoose.Schema({
-  categoryItems: { type: Array, required: true },
-  filterItems: { type: Boolean },
-  search: { type: String, required: true },
-  layout: { type: String, required: true },
-  description: { type: String, required: true },
-  quantity: { type: String, required: true },
-  checkbox: { type: Boolean },
-  checkbox2: { type: Boolean },
   name: { type: String, required: true },
+  duration: { type: String, required: true },
+  categories: { type: Array, required: true },
+  newsQuantity: { type: String, required: true },
+  carouselQuantity: { type: String, required: true },
+  layoutQuantity: { type: String, required: true },
+  videowallQuantity: { type: String, required: true },
+  search: { type: String },
+  carouselSearch: { type: String },
+  layoutSearch: { type: String },
+  layout: { type: Array, required: true },
+  layoutDescription: { type: String },
+  description: { type: String },
+  
 });
 
 module.exports = mongoose.model("Playlist", PlaylistSchema);
