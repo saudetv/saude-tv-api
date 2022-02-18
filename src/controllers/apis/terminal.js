999cd249bd359173465e01a3771b1e52e2c90060
matheus <matheus@kokua.digital>
2022-02-18 15:57:05 -0300

    feat:adiçao da rota copy

--- src/controllers/apis/terminal.js
+++ src/controllers/apis/terminal.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const TerminalServiceClass = require("../../services/terminals");
-const terminalService = new TerminalServiceClass();
-const authMiddleware = require("../../middleware/auth");
+const express = require('express');
+const TerminalServiceClass = require('../../services/terminals');
+const terminalService = new TerminalServiceClass;
+const authMiddleware = require('../../middleware/auth');
 let router = express.Router();
 
-router.get("/", authMiddleware, terminalService.index);
-router.get("/:id", terminalService.show);
-router.post("/", authMiddleware, terminalService.store);
-router.put("/:id", terminalService.update);
-router.delete("/:id", terminalService.destroy);
+router.get('/', authMiddleware, terminalService.index);
+router.get('/:id', terminalService.show);
+router.post('/', authMiddleware, terminalService.store);
+router.put('/:id', terminalService.update);
+router.delete('/:id', terminalService.destroy);
 router.patch("/:id", terminalService.update);
+router.copy("/:id", terminalService.store);
 
 module.exports = router;
