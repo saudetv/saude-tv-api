5008c75d0436f84adba41e0a15a6ebb34552b4aa
matheus <matheus@kokua.digital>
2022-02-22 18:02:15 -0300

    fix:models customer, inicio do copy

--- src/services/terminals/index.js
+++ src/services/terminals/index.js
@@ -1,5 +1,7 @@
 const Model = require("../../models/Terminal");
 const Service = require("../service");
+const errorHandler = require("../../helpers/errorHandler")
+const { validate, setReturnObject } = require("../../helpers/response");
 const Entity = "terminal";
 
 class Question extends Service {
@@ -25,6 +27,33 @@ class Question extends Service {
 
   destroy = (req, res) => {
     super.destroy(req, res);
+  };
+
+  copy = async (req, res) => {
+    try {
+      const _id = req.params.id
+    const terminal = await Model.findById(_id).lean().exec(async function(error,results){
+      if(!error){
+        const object = results
+        delete object._id
+        console.log(object)
+        const newTerminal = new Model(object)
+        newTerminal.save()
+        let result = await validate(
+          newTerminal,
+          Entity,
+          process.env.CODE_FOUND,
+          process.env.MESSAGE_FOUND
+        );
+        res.json(result);
+      }
+    })
+    
+    } catch (error) {
+      var result = await errorHandler(error, Entity)
+      res.status(result.statusCode).json(result);
+    }
+    
   };
 }
 
