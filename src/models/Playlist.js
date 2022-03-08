5fe0307133b805b31458583ad5e8082037a406c9
mauro <mauromaio.dev@gmail.com>
2022-03-08 13:45:41 -0300

    feat: tela playlists

--- src/models/Playlist.js
+++ src/models/Playlist.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const PlaylistSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  typeAdress: { type: String, required: true },
   saveasName: { type: String, required: true },
   duration: { type: String, required: true },
   categories: { type: Array, required: true },
-  newsQuantity: { type: String },
-  carouselQuantity: { type: String },
-  layoutQuantity: { type: String },
-  videowallQuantity: { type: String },
+  newsQuantity: { type: String, required: true },
+  carouselQuantity: { type: String, required: true },
+  layoutQuantity: { type: String, required: true },
+  videowallQuantity: { type: String, required: true },
   search: { type: String },
   carouselSearch: { type: String },
   layoutSearch: { type: String },
