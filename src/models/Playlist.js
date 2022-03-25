ee49982a2b7cf270669cae9b9f792fefa052182a
Vitor Meira Gomes <vitorg.dev@gmail.com>
2022-03-25 11:09:45 -0300

    fix: ajuste playlist

--- src/models/Playlist.js
+++ src/models/Playlist.js
@@ -1,30 +1,8 @@
 const mongoose = require("mongoose");
 
 const PlaylistSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  saveasName: { type: String, required: true },
-  duration: { type: String, required: true },
-  categories: { type: Array, required: true },
-  newsQuantity: { type: String },
-  carouselQuantity: { type: String },
-  layoutQuantity: { type: String },
-  videowallQuantity: { type: String },
-  search: { type: String },
-  carouselSearch: { type: String },
-  layoutSearch: { type: String },
-  layout: { type: Array },
-  layoutDescription: { type: String },
-  description: { type: String },
-  addPlaylistCardsImage: { type: String },
-  addPlaylistCardsName: { type: String, required: true },
-  addPlaylistCardsDuration: { type: String },
-  addPlaylistCardsCounter: { type: String },
-  cardTypeMedia: { type: String },
-  cardTypeCarousel: { type: String },
-  cardTypeNews: { type: String },
-  cardDate: { type: String, required: true },
-  cardLoops: { type: String, required: true },
-  cardTimer: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  contents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Content" }],
 });
 
 module.exports = mongoose.model("Playlist", PlaylistSchema);
