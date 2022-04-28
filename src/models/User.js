d6c85eacc775e96bdea7f01fcc5ffdff65aeb85e
matheus <matheus@kokua.digital>
2022-04-28 09:30:51 -0300

    feature/mudanças

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
-  type: { type: String, required: true },
   name: { type: String, required: true, trim: true },
   createdAt: { type: Date, default: Date.now() },
   status: { type: Boolean, required: true, default: true },
