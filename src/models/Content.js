05aa4e6bfbcadf5e06401aafb949cb600992c3a3
matheus <matheus@kokua.digital>
2022-04-28 11:20:15 -0300

    feature/contents

--- src/models/Content.js
+++ src/models/Content.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 
 const ContentSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  file: { type: String, required: true },
+  file: { type: Array, required: true },
   categories: { type: Array, required: true },
   startDate: { type: Date, default: Date.now() },
-  days: { type: Array, required: true },
-  agencies: { type: Array, required: true },
-  customers: { type: Array, required: true },
 });
 
 module.exports = mongoose.model("Content", ContentSchema);
