edfa02704059777159278cb051cefc410614c0ec
vitor <vitor>
2022-05-17 09:32:55 -0300

    fix: ajustes

--- src/models/Content.js
+++ src/models/Content.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const ContentSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  file: { type: String, required: true },
+  file: { type: String },
   categories: { type: Array, required: true },
-  initialDate: { type: String, required: true },
-  finalDate: { type: String, required: true },
+  initialDate: { type: String },
+  finalDate: { type: String },  
+  url: { type: String },
+  type: { type: String, enum: ['RSS', 'VIDEO'], default: 'VIDEO'}
 });
 
 module.exports = mongoose.model("Content", ContentSchema);
