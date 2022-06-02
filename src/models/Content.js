4be9fe2a54e194d5a27a1f8cef6365e3e6765175
Felipe <fcastro.dev@gmail.com>
2022-06-02 16:00:31 -0300

    feat:Instagran

--- src/models/Content.js
+++ src/models/Content.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose");
 
-const ContentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  file: { type: String },
-  thumbnail: { type: String },
-  initialDate: { type: String },
-  finalDate: { type: String },
-  url: { type: String },
-  duration: { type: Number },
-  rss: [{
-    title: { type: Array },
-    description: { type: Array },
-    link: { type: Array },
-    pubDate: { type: Array },
-    category: { type: Array },
-    "media:content": { type: Array },
-    "content:encoded": { type: Array },
-    guid: { type: Array }
-  }],
-  type: { type: String, enum: ['RSS', 'VIDEO'], default: 'VIDEO' }
-},
+const ContentSchema = new mongoose.Schema(
   {
-    timestamps: true
-  });
+    name: { type: String, required: true },
+    file: { type: String },
+    thumbnail: { type: String },
+    initialDate: { type: String },
+    finalDate: { type: String },
+    url: { type: String },
+    duration: { type: Number },
+    rss: [
+      {
+        title: { type: Array },
+        description: { type: Array },
+        link: { type: Array },
+        pubDate: { type: Array },
+        category: { type: Array },
+        "media:content": { type: Array },
+        "content:encoded": { type: Array },
+        guid: { type: Array },
+      },
+    ],
+    type: {
+      type: String,
+      enum: ["RSS", "VIDEO", "INSTAGRAM"],
+      default: "VIDEO",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Content", ContentSchema);
--- src/models/Instagram.js
+++ src/models/Instagram.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const InstagramSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  instagram: { type: String, required: true },
+  url: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Instagram", InstagramSchema);
