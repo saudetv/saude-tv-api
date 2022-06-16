f4bacfa4527bb3437d70da7b0bc64150e49d7b78
Vitor <vitorg.dev@gmail.com>
2022-06-16 01:57:21 -0300

    FIX: TEmpo da JOB Para 1hr

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -3,6 +3,6 @@ const cron = require('node-cron');
 const RssServiceClass = require('../services/rss');
 const rssService = new RssServiceClass;
 
-cron.schedule('*/59 * * * *', async () => {
+cron.schedule('*/50 * * * *', async () => {
     await rssService.getRssData()
 });
