5a3943f09addfefc25cda5309a630b1c4e1bd337
Vitor Meira Gomes <vitorg.dev@gmail.com>
2022-06-22 18:18:29 -0300

    final

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -3,6 +3,6 @@ const cron = require('node-cron');
 const RssServiceClass = require('../services/rss');
 const rssService = new RssServiceClass;
 
-cron.schedule('* * * *', async () => {
+cron.schedule('*/59 * * * *', async () => {
     await rssService.getRssData()
 });
