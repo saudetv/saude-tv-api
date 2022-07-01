ff28d77aadead881394d739af5a11a56dce0c24b
Vitor Meira Gomes <vitorg.dev@gmail.com>
2022-07-01 16:15:04 -0300

    Finalizado

--- src/helpers/s3.js
+++ src/helpers/s3.js
@@ -2,35 +2,40 @@
 const { s3Bucket } = require('../config/s3')
 
 const uploadBase64 = async (bucket, fileName, file) => {
-    const buffer = Buffer.from(file.replace(/^data:video\/\w+;base64,/, ""), 'base64')
-    const type = file.split(';')[0].split('/')[1];
-    const data = {
-        Bucket: bucket,
-        Key: `${fileName}.${type}`,
-        Body: buffer,
-        ContentEncoding: 'base64',
-        ContentType: `video/${type}`
-    };
-    const image = await s3Bucket.putObject(data, function (err, data) {
-        if (err) {
-            console.log(err);
-            console.log('Error uploading data: ', data);
-        } else {
-            console.log('successfully uploaded the image!');
-        }
-    });
-    return image
+  const buffer = Buffer.from(file.replace(/^data:video\/\w+;base64,/, ""), 'base64')
+  const type = file.split(';')[0].split('/')[1];
+  const data = {
+    Bucket: bucket,
+    Key: `${fileName}.${type}`,
+    Body: buffer,
+    ContentEncoding: 'base64',
+    ContentType: `video/${type}`
+  };
+  const image = await s3Bucket.putObject(data, function (err, data) {
+    if (err) {
+      console.log(err);
+      console.log('Error uploading data: ', data);
+    } else {
+      console.log('successfully uploaded the image!');
+    }
+  });
+  return image
 }
 
 const getObjectFromS3 = async (bucket, fileName) => {
+  try {
     const data = {
-        Bucket: bucket,
-        Key: `${fileName}.mp4`,
+      Bucket: bucket,
+      Key: `${fileName}.mp4`,
     };
     const file = await s3Bucket.getObject(data).promise();
     const src = "data:video/mp4;base64," + file.Body.toString('base64');
 
     return src;
+  } catch (error) {
+    console.error(error);
+  }
+
 }
 
 module.exports = { uploadBase64, getObjectFromS3 }
