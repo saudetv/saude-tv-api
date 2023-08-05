381c3771640fa83b533f3413a7a9a93c29f76c91
Vitor <vitorg.dev@gmail.com>
2023-08-05 02:47:18 -0300

    chiore: add email in model

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -36,6 +36,7 @@ const CustomerSchema = new mongoose.Schema(
     fantasyName: { type: String, required: true },
     cnpj: { type: String },
     acting: { type: String },
+    email: { type: String },
     address: { type: String },
     number: { type: String },
     complement: { type: String },
