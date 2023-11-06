c20935b72ecc2c6c1bdfab72fc6e3a4d346c9080
VITOR GOMES <vitorg.dev@gmail.com>
2023-11-06 12:08:09 -0300

    fix: node loader exp

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@ module.exports = {
   apps: [
     {
       name: "SAUDE-TV-API",
-      script: "server.js",
+      script: "YOUR_MAIN_FILENAME.js",
       watch: true,
+      interpreter_args:
+        "--experimental-loader=newrelic/esm-loader.mjs --trace-warnings",
       env: {
         NEW_RELIC_APP_NAME: "SAUDE-TV-API",
         NEW_RELIC_LICENSE_KEY: "0aeaae854caf04d829f3f179be5710f4FFFFNRAL",
-        NODE_OPTIONS: "--experimental-loader=newrelic/esm-loader.mjs",
       },
     },
   ],
