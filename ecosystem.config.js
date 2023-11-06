2c34d32e482d3be9d734f2802bb36e6ff9ca3410
VITOR GOMES <vitorg.dev@gmail.com>
2023-11-06 12:02:00 -0300

    fix: new relic

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "SAUDE-TV-API",
+      script: "server.js",
+      watch: true,
+      env: {
+        NEW_RELIC_APP_NAME: "SAUDE-TV-API",
+        NEW_RELIC_LICENSE_KEY: "0aeaae854caf04d829f3f179be5710f4FFFFNRAL",
+        NODE_OPTIONS: "--experimental-loader=newrelic/esm-loader.mjs",
+      },
+    },
+  ],
+};
